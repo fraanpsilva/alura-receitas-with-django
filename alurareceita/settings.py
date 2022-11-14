8c006787d88a88d33f67ce533b04ac88c80ac83a
fraanpsilva <francilenesilva.pt@gmail.com>
2022-11-14 19:13:19 +0000

    models e banco de dados

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receitas',
+        'USER' :'postgres',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost'
     }
 }
 
