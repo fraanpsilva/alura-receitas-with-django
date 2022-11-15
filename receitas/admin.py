dd0d77bc7feaa9537700d99301045d48d7b877e4
fraanpsilva <francilenesilva.pt@gmail.com>
2022-11-15 11:09:37 +0000

    ajustes djando-admin, busca, filtros, paginação

--- receitas/admin.py
+++ receitas/admin.py
@@ -4,4 +4,14 @@ from .models import Receita
 # Register your models here.
 # https://docs.djangoproject.com/en/3.0/ref/contrib/admin/
 
-admin.site.register(Receita)
+# listando as receitas no admin
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria')
+    list_display_links = ('id', 'nome_receita')
+    search_fields = ('nome_receita',) # como pesquisar
+    list_filter = ('categoria',) # filtros
+    list_per_page = 10 # paginação
+
+
+
+admin.site.register(Receita, ListandoReceitas)
